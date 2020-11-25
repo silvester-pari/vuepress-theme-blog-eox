224467949661f6172db0a61062fae1724c92f864
paris <silvester.pari@eox.at>
2020-11-25 16:44:38 +0100

    add teampage layout

---
+++ layouts/TeamPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <BaseLayout>
+    <template #content>
+      <div>TEAMPAGE</div>
+      <div>{{$pagination}}</div>
+      <div>{{$page}}</div>
+    </template>
+    <template #sticky>
+      <Sticky />
+    </template>
+  </BaseLayout>
+</template>
