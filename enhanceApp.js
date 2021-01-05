0f7344ce85da3d6321783f40f1a8d6ad1aa01161
paris <silvester.pari@eox.at>
2021-01-05 16:52:20 +0100

    directly pass vuetify options to enhanceapp

--- enhanceApp.js
+++ enhanceApp.js
@@ -32,7 +32,25 @@ export default ({
   siteData // site metadata
 }) => {
   Vue.use(Vuetify)
-  options.vuetify = new Vuetify({})
+  options.vuetify = new Vuetify({
+    theme: {
+      options: {
+        customProperties: true,
+      },
+      themes: {
+        light: {
+          primary: '#004170',
+          secondary: '#424242',
+          accent: '#82B1FF',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FFC107'
+        },
+      },
+    },
+  })
+  // options.vuetify = new Vuetify({})
 
   Vue.use(VueScrollactive)
 
